1f988d883b55c9e06fe17cc8c99fa34ca077c2f8
kyawthihalin <kyawthihapersonal@gmail.com>
2024-09-01 16:13:45 +0630

    update

--- prisma/seeds/CarbonFootprintSeeder.ts
+++ prisma/seeds/CarbonFootprintSeeder.ts
@@ -5,11 +5,11 @@ const prisma = new PrismaClient();
 async function CarbonFootprintSeeder() {
   await prisma.carbonFootprint.createMany({
     data: [
-      { date: new Date(), value: 123.45 },
-      { date: new Date(), value: 67.89 },
-      { date: new Date(), value: 62.89 },
-      { date: new Date(), value: 40.89 },
+      { date: new Date(), value: 40.45 },
+      { date: new Date(), value: 29.89 },
       { date: new Date(), value: 10.89 },
+      { date: new Date(), value: 42.89 },
+      { date: new Date(), value: 32.89 },
       { date: new Date(), value: 2.89 },
     ],
   });
